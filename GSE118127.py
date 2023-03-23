0e791ef7cf6a9a2e230132ff1e5a85900113ca1b
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-23 15:17:08 +0000

    Update GSE118127.py

--- GSE118127.py
+++ GSE118127.py
@@ -6,8 +6,10 @@ dataA = sc.read_h5ad('local.h5ad')
 # extract raw count data
 raw = dataA.raw.to_adata()
 raw.to_df().iloc[:]
+raw = raw.to_df()
 
 # Test this
 # Code for doing the python equivalent of R's table() apparently
+# Don't really need this if using scanpy as it gives a summary at the bottom
 df = pd.read_clipboard()
 df.Gender.value_counts()   
