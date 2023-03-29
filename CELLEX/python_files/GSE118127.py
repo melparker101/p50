9c09a5d626c2c9423c5ddba55e1ff04bf517f4cf
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-03-29 16:42:10 +0100

    Update GSE118127.py

--- CELLEX/python_files/GSE118127.py
+++ CELLEX/python_files/GSE118127.py
@@ -1,15 +1,50 @@
+############################################################
+# This script is for running CELLEX on dataset GSE118127.
+# melodyjparker14@gmail.com
+# Use the cellex conda environment.
+############################################################
+
 import scanpy as sc
+import pandas as pd
+import cellex
 
-# read in data
-dataA = sc.read_h5ad('local.h5ad')
+# Read in data
+adata = sc.read_h5ad('local.h5ad')
 
-# extract raw count data
-raw = dataA.raw.to_adata()
-raw.to_df().iloc[:]
-raw = raw.to_df()
+# Extract metadata
+metadata = adata.obs["cell_type"]
+metadata = pd.DataFrame(metadata)
 
-# Test this
-# Code for doing the python equivalent of R's table() apparently
-# Don't really need this if using scanpy as it gives a summary at the bottom
-df = pd.read_clipboard()
-df.Gender.value_counts()   
+# Extract raw count data
+raw = adata.raw.to_adata()
+raw = raw.transpose()
+mat = raw.to_df()
+mat
+
+print(metadata.groupby('cell_type').cell_type.count())
+
+# Run CELLEX
+# Compute ESO object
+eso = cellex.ESObject(data=mat, annotation=metadata, verbose=True)
+
+# Compute ESw, ESw* and ESmu
+eso.compute(verbose=True)
+
+# Save and inspect results
+# Save expression specificity mu and sd matrix for gene symbols
+eso.save_as_csv(file_prefix=prefixData_sym, path=dirOut, verbose=True)
+eso.results["esmu"].head()
+
+# Map symbols to ensembl ids
+cellex.utils.mapping.human_symbol_to_human_ens(eso.results["esmu"], drop_unmapped=True, verbose=True)
+eso.results["esmu"].head()
+
+# Save expression specificity matrix for gene ensembl ids
+eso.save_as_csv(file_prefix=prefixData_ens, path=dirOut, verbose=True)
+
+# Other save options
+# eso.save_as_csv(keys=["all"], verbose=True)  # Save all results
+# eso.results["esmu"].to_csv("esmu/" + "GSE202601.esmu.csv.gz")
+
+# Delete object to release memory
+del eso
