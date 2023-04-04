0ccd7e93d789d6ce209c0025509d3acdf660d187
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-04 18:41:59 +0100

    Update filter_sumstats.R

--- CELLECT/filter_sumstats.R
+++ CELLECT/filter_sumstats.R
@@ -1,59 +1,30 @@
-# Use a high number of cores (e.g. 10)
+#############################################################
+## Filter Sumstats
+## melodyjparker14@gmail.com - April 2023
+## This script adds a MarkerName column to sumstats data and then
+## Use a high number of cores (e.g. 10)
+#############################################################
 
-# Load in libraries
+##############################
+# 1 - Load librairies
+##############################
 library(data.table)
 library(stringr)
-
-# Read in infertility sumstats
-filename <- 'female_infertility_analysis1_UKBB_Finngen_EstBB_GandH_noMACfilter_March20231.out'
-Infertility1_F_Finngen <- fread(filename)
-
-# Read in hormone sumstats
-hormone_sumstats <- list()
-for(i in seq_along(filelist)) {
-  hormone_sumstats[] <- name
-}
-
-
-filelist <- list.files(pattern='.txt')
-# hormone_sumstats <- list()
-for(i in seq_along(filelist)){
-  name <- str_replace(filelist[i],"_filtered.txt","")
-  sumstats <- fread(filelist[i])
-  assign(name, sumstats)
-  hormone_sumstats <- append(hormone_sumstats,name)
-}
-
-# Clean up
-rm(sumstats)
-
-# Make a MarkerName column for hormone sumstats
-# Columns in hormome sumstats include: CHROM, GENPOS, Allele1, Allele2
-# touppper() is probably not necessary but leave in for robustness
-for(file in seq_along(filelist)) {
-  sumstats <- str_replace(filelist[file],"_filtered.txt","")
-  n <- nrow(sumstats)
-  for (i in 1:n){
-    	a1 <- toupper(sumstats$Allele1[i])
-	    a2 <- toupper(sumstats$Allele2[i])
-      chrom <- sumstats$CHROM[i]
-      position <- sumstats$GENPOS[i]
-      alleles <- paste(sort(c(a1,a2)), collapse="_")
-      sumstats[i,"key"]<- paste(chrom,":",position,"_",alleles,sep="")
-    }
-}
-
 library(dplyr)
-# Try to do this with dplyr()
-df_before <- df %>% 
-   mutate(a1=sum(a+1), .before = )
+library(purrr)
 
+############################## 
+# 2 - Source files
+##############################
+# Infertility summary stats
+filename <- 'female_infertility_analysis1_UKBB_Finngen_EstBB_GandH_noMACfilter_March20231.out'
+# Hormones summary stats
+filelist <- list.files(pattern='.txt')
 
-FSH_F_EUR <- FSH_F_EUR %>% 
-              mutate(MarkerName=paste(CHROM,":",position,"_",alleles,sep=""))
-
+##############################
+# 3 - Define functions
+##############################
 # Make a function for sorting alleles in alphabetical order and joining with an underscore
-
 sort_alleles <- function(a1,a2) {
   a1 <- toupper(a1)
   a2 <- toupper(a2)
@@ -61,12 +32,29 @@ sort_alleles <- function(a1,a2) {
   return(sorted_alleles)
 }
 
-sort_alleles <- function(a1, a2) {
-  a1 <- toupper(a1)
-  a2 <- toupper(a2)
-  sorted_alleles <- paste(sort(c(a1, a2))[1:2], collapse = "_")
-  return(sorted_alleles)
+##############################
+# 4 - Start main code
+##############################
+# Read in infertility sumstats
+Infertility1_F_Finngen <- fread(filename)
+
+# Read in hormone sumstats
+for(i in seq_along(filelist)){
+  name <- str_replace(filelist[i],"_filtered.txt","")
+  sumstats <- fread(filelist[i])
+  assign(name, sumstats)
 }
+
+# Clean up
+rm(sumstats)
+
+#####################################
+
+FSH_F_EUR <- FSH_F_EUR %>% 
+              mutate(MarkerName=paste(CHROM,":",position,"_",alleles,sep=""))
+
+
+FSH_F_EUR
 
 test %>% mutate(MarkerName=paste(CHROM,GENPOS,sort_alleles(Allele1,Allele2), sep=":"), .before = ID)
 
@@ -76,19 +64,116 @@ test <- as.data.frame(test %>%
   rowwise() %>%
   mutate(MarkerName = paste(CHROM, GENPOS, sort_alleles(Allele1, Allele2), sep = ":"), .before = ID))
 
+
+
 #########################
-test %>% 
-  mutate(MarkerName = paste(CHROM, GENPOS, purrr::pmap_dbl(list(Allele1, Allele2), sort_alleles) , sep = ":"), .before = ID)
-
-test %>% 
-  mutate(MarkerName = paste(CHROM, GENPOS, map2_chr(Allele1, Allele2, sort_alleles), sort_alleles), sep = ":"), .before = ID)
-
-test %>% 
-  mutate(MarkerName = map2_chr(Allele1, Allele2, sort_alleles), sep = ":"), .before = ID)
 
 test <- head(FSH_F_EUR)
 
-FSH_F_EUR %>% mutate(MarkerName=paste(CHROM,GENPOS,sep=":",sort_alleles(Allele1,Allele2)), .before = ID)
+test %>% 
+  mutate(MarkerName = paste(CHROM,GENPOS,map2_chr(Allele1, Allele2, sort_alleles), sep = ":"), .before = ID)
+#########
+# Do a comparison test of both of these
 
-# debug
+start <- Sys.time()
+FSH_F_EUR_edited <- as.data.frame(FSH_F_EUR %>%
+  rowwise() %>%
+  mutate(MarkerName = paste(CHROM, GENPOS, sort_alleles(Allele1, Allele2), sep = ":"), .before = ID))
+print( Sys.time() - start )
 
+# Time difference of 18.23314 mins
+
+start <- Sys.time()
+FSH_F_EUR_edited2 <- FSH_F_EUR %>% 
+  mutate(MarkerName = paste(CHROM,GENPOS,map2_chr(Allele1, Allele2, sort_alleles), sep = ":"), .before = ID)
+print( Sys.time() - start )
+
+# Time difference of 14.10276 mins
+
+# Still a bit slow but we can just send them off
+# An awk command will be too complicated and hard to read/understand for anyone else
+
+
+
+# Try it with data.table
+FSH_F_EUR_edited_dt <- FSH_F_EUR 
+
+start <- Sys.time()
+FSH_F_EUR_edited_dt[, MarkerName := paste(CHROM, GENPOS, unlist(Map(sort_alleles, Allele1, Allele2)), sep = ":")]
+print( Sys.time() - start )
+
+test[, MarkerName := paste(CHROM,GENPOS,map2_chr(Allele1, Allele2, sort_alleles), sep = ":"), .before = ID]
+test[, MarkerName := paste(CHROM,GENPOS, sort_alleles(Allele1, Allele2), sep = ":"), .before = ID]
+
+ DT <- data.table(New = c(1:10), DT)
+DT[New = rpois(20,2)]
+
+
+# add a new column as the first column
+test[, MarkerName:= paste(CHROM,GENPOS, do.call(sort_alleles(Allele1, Allele2)), sep = ":")]
+test <- test[, c(MarkerName,colnames(test)[-1])]
+
+# view the updated data table
+dt
+
+pmin <- function (...) UseMethod("pmin")
+pmin.default <- function (...) {
+  res <- .Primitive("pmin")(...)
+  if (anyNA(res)) res[is.na(res)] <- -Inf
+  res
+}
+#######################
+#try this
+FSH_F_EUR_edited_dt <- FSH_F_EUR 
+
+sort_alleles_vec <- function(a1, a2) {
+  a1 <- toupper(a1)
+  a2 <- toupper(a2)
+  alleles <- matrix(c(a1, a2), ncol = 2)
+  sorted_alleles <- apply(alleles, 1, function(x) paste(sort(x), collapse = "_"))
+  return(sorted_alleles)
+}
+			  
+start <- Sys.time()
+FSH_F_EUR_edited_dt[, MarkerName := paste(CHROM, GENPOS, sort_alleles_vec(Allele1, Allele2), sep = ":")]
+print( Sys.time() - start )
+# Time difference of 10.81209 mins
+
+start <- Sys.time()
+FSH_F_EUR_edited_dt[, MarkerName := paste(CHROM, GENPOS, sort_alleles_vec(Allele1, Allele2), sep = ":")]
+print( Sys.time() - start )			  
+
+# Other suggestions
+			  
+# 1.
+set(test, j = "MarkerName", value = paste(test$CHROM, test$GENPOS, sort_alleles(test$Allele1, test$Allele2), sep = ":"))
+			  
+# 2. Use stringr's str_c() function: The str_c() function from the stringr package is a faster alternative to base R's paste() function. 
+set(test, j = "MarkerName", value = str_c(test$CHROM, ":", test$GENPOS, ":", sort_alleles(test$Allele1, test$Allele2)))
+			 
+#3. Subset the data
+subset_test <- test[, .(CHROM, GENPOS, Allele1, Allele2)]
+set(subset_test, j = "MarkerName", value = paste(subset_test$CHROM, subset_test$GENPOS, sort_alleles(subset_test$Allele1, subset_test$Allele2), sep = ":"))
+set(test, i = which(test$MarkerName %in% subset_test$MarkerName), j = "MarkerName", value = subset_test$MarkerName)
+
+			  
+			  
+# Try 2
+start <- Sys.time()			  
+set(FSH_F_EUR_set, j = "MarkerName", value = str_c(FSH_F_EUR_set$CHROM, ":", FSH_F_EUR_set$GENPOS, ":", sort_alleles(FSH_F_EUR_set$Allele1, FSH_F_EUR_set$Allele2)))
+print( Sys.time() - start )	
+# took ages so I shopped it			  
+			  
+############################
+			  
+start <- Sys.time()
+FSH_F_EUR_edited_dt[, MarkerName := paste(CHROM, GENPOS, unlist(Map(sort_alleles, Allele1, Allele2)), sep = ":")]
+print( Sys.time() - start )
+# Time difference of 16.78605 mins
+			  
+
+############
+# MAF > 1%
+dt <- data.table(dt)
+dt[, MAF := pmin(Freq1, 1-Freq1)]
+dt %>% filter(MAF>0.01)
