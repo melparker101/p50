00656f140b4683c0fe3c207e7d6301134e3ac3a2
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-05 17:27:30 +0100

    Update add_rsid.sh

--- CELLECT/add_rsid.sh
+++ CELLECT/add_rsid.sh
@@ -11,6 +11,7 @@
 #SBATCH -J add_rsid
 #SBATCH -o logs/output.out
 #SBATCH -e logs/error.err
+#SBATCH -a 1-7
 
 #  Parallel environment settings 
 #  For more information on these please see the documentation 
@@ -28,8 +29,32 @@ echo "Started at: "`date`
 echo "##########################################################"
 
 
-awk 'NR==1 {print "rsid",$0; next} NR==FNR {a[$1]=$0; next} $1 in a {print $2,a[$1]}' test_ss.txt ../dbSNP/MarkerName_map_GRCh37.txt > female_infertility_UKBB.txt
+# Set working directory
+cd = //well/lindgren/users/mzf347/p50
+
+# Set variables
+IN=sumstats/MAF_filtered
+OUT=sumstats/premunge
+MAP=dbSNP
+
 # Use GRCh37_GCF_000001405.25_map.txt to get more rsids
+
+# Make an index file if it doesn't exist
+if [ ! -f "$IN"/index.txt ]; then
+  for f in "$IN"/*; do basename ${f} >> "$IN"/index.txt; done
+fi
+
+if [ ! -p "$OUT" ]; then
+  mkdir "$OUT"
+fi
+
+# Set sumstats file name for each slurm array task
+SUMSTATS_FILE=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' "$IN"/index.txt)
+
+# Add rsid column and save in new folder
+awk 'NR==1 {print "rsid",$0; next} NR==FNR {a[$1]=$0; next} $1 in a {print $2,a[$1]}' \
+    "$IN"/"$SUMSTATS_FILE" "$MAP"/MarkerName_map_GRCh37.txt > \
+    "$OUT"/premunge_"${SUMSTATS_FILE##*MAFfiltered_}"
 
 
 echo "###########################################################"
