45d8b1854d5665a80774f3c458e50471b3d9cf8d
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-11 14:06:23 +0100

    Create edit_yaml.sh

---
+++ CELLECT/edit_yaml.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+########################################################################
+# This script uses sed to create a CELLECT config yaml file per phenotype
+# It copies and edits the template config file 'config_template.yml'
+########################################################################
+
+mkdir -p config
+
+for phenotype in $(cut -f1 sumstats/sample_size.txt)
+do
+  sumstats_file=munged_"$phenotype".sumstats.gz
+  config_out=config/config_"$phenotype".yml
+  
+  # Add backslashes before special characters ready for double quoted sed
+  # sumstats_file=${sumstats_file////\\/}
+  
+  # Copy the template
+  cp config_template.yml "$config_out"
+  
+  # Edit yaml
+  # Use double quotes to expand out the shell variables
+  sed -i "s/<phenotype>/$phenotype/" $config_out
+  sed -i "s/<sumstats_id>/$phenotype/" $config_out
+  sed -i "s/<sumstats_file>/$sumstats_file/" $config_out
+done
