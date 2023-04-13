bf6c2a76033ac25ada95cc7755261509978cac9f
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-13 18:34:53 +0100

    Update plot_results.R

--- visualisation/plot_results.R
+++ visualisation/plot_results.R
@@ -8,36 +8,24 @@
 ##############################
 # 0 - Load librairies
 ##############################
-library(dplyr)  # for mutate 
+library(dplyr)
 library(data.table)
+library(stringr)
 
 ############################## 
 # 1 - Choose data
 ##############################
-# edit this based on chosen data/phenotypes
-
-# phenotypes need to be consistent with CELLECT output directory names
-# for the alternative annotations, add "_ALT" on the end
-
+# Extract phenotypes from index file
+# Store in a list called 'phenotypes'
 phenotype_table <- fread("sumstats/sample_size.txt", select = 1)
-
 phenotypes <- c()
 for(i in phenotype_table){phenotypes <- append(phenotypes,i)}
 
 ############################## 
 # 2 - Source file
 ##############################
-# paste general results data file
-data_type <- paste("CELLECT", toupper(data_source), toupper(tissue_type), toupper(RNAseq_type), sep="-")
-# "CELLECT-ATLAS-ADIPO-SN"
-
-# input files need to be inside loop in main code
+# Input file name is created inside loop
 output_file <- paste("CELLECT/plots/", data_source, "_", tissue_type, "_", RNAseq_type, sep="")
-
-# CELLECT output directory
-library(stringr)
-
-datasets <- list.files("counts")
 
 ##############################
 # 3 - Define functions
@@ -50,12 +38,14 @@ resultsCol <- function(x) {
 ##############################
 # 4 - Start code
 ##############################
+# Dataset acc. numbers
+datasets <- list.files("counts")
 
 for (dataset in datasets){
 
-  # Extract last three characters of dataset
+  # Extract last three characters of dataset acc number
   ds <- str_sub(dataset,-3,-1)
-
+  # CELLECT output are in the format CELLECT_OUT_123
   cellect_out_dir <- paste0('CELLECT_OUT_',ds)
 
   # Create a joint results table for a dataset
@@ -72,7 +62,8 @@ for (dataset in datasets){
       joint_results <- prioritization[,c(3,7)]    
     }
   }
-
+  
+  # Transfer df to matrix
   joint_results <- joint_results[order(tolower(joint_results$annotation)),]
   rownames(joint_results) <- joint_results[,1]
   joint_results <- joint_results[,-1]
@@ -88,19 +79,20 @@ for (dataset in datasets){
 
   output_file <- paste0('plots/CELLECT_plot_hormones_',dataset,".pdf")
 
-    # Generate barplot
-    title <- paste0("CELLECT results with hormones sumstats and dataset ",dataset)
+  # Generate barplot
+  title <- paste0("CELLECT results with hormones sumstats and dataset ",dataset)
 
-    pdf(file=output_file, width=30)
-    {par(lwd = 2)
-    barplot(matrix, space=c(0,8), width = 1, border=T, beside = TRUE, col = terrain.colors(n), xlim = c(6, y_lim +150), ylim = c(0, y_lim), legend = TRUE, args.legend = list(ncol = 4, bty = "n", xjust=1, x = "top"))
-    # abline(h=b_sig,lty = 2)
-    title(ylab = '-log(p value)', line=2.2, font.lab=1)
-    title(main = title, adj = 0.5)
-    abline(h=0)
-    }
- 
-    dev.off()
+  pdf(file=output_file, width=30)
+  {par(lwd = 2)
+  barplot(matrix, space=c(0,8), width = 1, border=T, beside = TRUE, col = terrain.colors(n), xlim = c(6, y_lim +150), ylim = c(0, y_lim), legend = TRUE, args.legend = list(ncol = 4, bty = "n", xjust=1, x = "top"))
+  # abline(h=b_sig,lty = 2)
+  title(ylab = '-log(p value)', line=2.2, font.lab=1)
+  title(main = title, adj = 0.5)
+  abline(h=0)
+  }
+
+  dev.off()
+  
   rm(matrix)
     
 }
