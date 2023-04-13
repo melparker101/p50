7b3421eb99e4acfe2666e276681ccdadfdd956af
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-13 11:39:53 +0100

    Update munge_sumstats.sh

--- CELLECT/munge_sumstats.sh
+++ CELLECT/munge_sumstats.sh
@@ -41,6 +41,8 @@ conda activate munge_ldsc
 # Define paths
 SUMSTATS=sumstats  # //well/lindgren/users/mzf347/p50/sumstats/
 CELLECT=CELLECT  # //well/lindgren/users/mzf347/p50/CELLECT
+IN=premunge
+OUT=munged_for_magma
 
 # Use sample_size.txt as an index file. col1 = text files, col2 = max sample sizes
 # -v is to pass an external shell variables to an awk; NR is for line number; {print $j} is to print column j
@@ -48,15 +50,16 @@ SUMSTATS_FILE=$(awk -v i="${SLURM_ARRAY_TASK_ID}" 'NR==i {print $1}' "$SUMSTATS"
 SAMPLE_SIZE=$(awk -v i="${SLURM_ARRAY_TASK_ID}" 'NR==i {print $2}' "$SUMSTATS"/sample_size.txt)
 
 # Make outdir
-mkdir -p "$SUMSTATS"/munged
+mkdir -p "$SUMSTATS"/"$OUT"
 
 # Run munge script
 python "$CELLECT"/ldsc/mtag_munge.py \
---sumstats "$SUMSTATS"/premunge/premunge_"$SUMSTATS_FILE".txt \
+--sumstats "$SUMSTATS"/"$IN"/premunge_"$SUMSTATS_FILE".txt \
 --merge-alleles "$CELLECT"/data/ldsc/w_hm3.snplist \
 --n-value "$SAMPLE_SIZE" \
+--keep-pval \
 --ignore MarkerName \
---out "$SUMSTATS"/munged/munged_"$SUMSTATS_FILE"
+--out "$SUMSTATS"/"$OUT"/munged_"$SUMSTATS_FILE"
 
 echo "###########################################################"
 echo "Array Task ID: $SLURM_ARRAY_TASK_ID"
