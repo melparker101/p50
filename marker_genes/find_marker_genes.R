2597983a3a2ef28172e60758de46f8c1dab7b00e
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-18 00:57:47 +0100

    Update find_marker_genes.R

--- marker_genes/find_marker_genes.R
+++ marker_genes/find_marker_genes.R
@@ -1,5 +1,8 @@
+########## Some of this is unnecessary #################
+
 library(Seurat)
 library(SeuratDisk)
+library(SeuratObject)
 
 GSE118127  GSE202601  GSE213216
 
@@ -7,39 +10,81 @@ GSE118127  GSE202601  GSE213216
 Convert("counts/GSE118127/local.gzip.h5ad", dest = "h5seurat", overwrite = FALSE)
 
 GSE118127 <- LoadH5Seurat("counts/GSE118127/local.gzip.h5seurat", verbose = T, misc=F)
-
 GSE118127.loom <- as.loom("counts/GSE118127/local.h5ad.loom", filename = "../output/pbmc3k.loom", verbose = FALSE)
 
-
-l6.immune <- Connect(filename = "../data/l6_r1_immune_cells.loom", mode = "r")
-l6.immune
-l6.seurat <- as.Seurat(l6.immune)
-l6.immune$close_all()
-
 GSE118127 <- Connect(filename = "counts/GSE118127/local.h5ad.loom", mode = "r")
-GSE118127
+GSE118127   
 # GSE118127.seurat <- as.Seurat(GSE118127)
 GSE118127.seurat <- as.Seurat(GSE118127, features = "feature_name",cells = "barcode")
 GSE118127$close_all()
 GSE118127.seurat
 
-# now use markergene funcions
+# Or we can just directly download the rsd 
+# https://cellxgene.cziscience.com/collections/2902f08c-f83c-470e-a541-e463e25e5058
 
-LoadH5Seurat("temp.h5seurat", verbose = T)
+GSE118127.seurat <- readRDS(file = "counts/GSE118127/local.rds")
+GSE118127.seurat 
 
-# https://github.com/mojaveazure/seurat-disk/issues/7
+##### We have 19 clusters for GSE118127 #####
+
+# https://satijalab.org/seurat/articles/pbmc3k_tutorial.html
+GSE118127.seurat <- FindVariableFeatures(GSE118127.seurat, selection.method = "vst", nfeatures = 2000)
+top10 <- head(VariableFeatures(GSE118127.seurat), 10)
+# Plot with and without labels
+plot1 <- VariableFeaturePlot(GSE118127.seurat)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot1 + plot2
+
+# PCA
+all.genes <- rownames(GSE118127.seurat)
+GSE118127.seurat <- ScaleData(GSE118127.seurat, features = all.genes)
+# perform PCA
+GSE118127.seurat <- RunPCA(GSE118127.seurat, features = VariableFeatures(object = GSE118127.seurat))
+print(GSE118127.seurat[["pca"]], dims = 1:5, nfeatures = 5)
+
+# Visualise PCA
+VizDimLoadings(GSE118127.seurat, dims = 1:2, reduction = "pca")
+DimPlot(GSE118127.seurat, reduction = "pca")
+
+# UMAP
+GSE118127.seurat <- RunUMAP(GSE118127.seurat, dims = 1:10)
+DimPlot(GSE118127.seurat, reduction = "umap")
+
+
+
+GSE118127.seurat[[c('cluster_id','cell_description','cell_type')]][1:20,]
+table(GSE118127.seurat[['cluster_id']])
+
+
+levels(GSE118127.seurat)
+
+# Set identity classes to an existing column in meta data
+Idents(object = GSE118127.seurat) <- "cell_description"
+levels(GSE118127.seurat)
+
+out_dir <- "cluster_markers/GSE118127"
+dir.create(out_dir)
+
+cell_type_list <- levels(GSE118127.seurat)
+for (cell_type in cell_type_list){
+  name <- paste(cell_type,"markers",sep="_")
+  name <- sub(" ", "_", name)
+  name <- sub(")", "", name)
+  name <- sub("-", "_", name)
+  value <- FindMarkers(GSE118127.seurat, ident.1 = cell_type)
+  assign(name, value)
+  out <- paste(out_dir,name,sep="/")
+  write.table(value,out,sep="\t")
+}
+  
+
 
 GSE202601
 
 GSE213216
 
-seuratObject <- LoadH5Seurat("example_dir/example_ad.h5Seurat"
-
+seuratObject <- LoadH5Seurat("example_dir/example_ad.h5Seurat")
 # Convert to Seurat ob
 Convert("GSE118127/local.h5ad", dest = "h5seurat", overwrite = False)
 pbmc3k <- LoadH5Seurat("pbmc3k_final.h5seurat")
 pbmc3k
-#> An object of class Seurat 
-#> 13714 features across 2638 samples within 1 assay 
-#> Active assay: RNA (13714 features, 0 variable features)
-#>  2 dimensional reductions calculated: pca, umap
