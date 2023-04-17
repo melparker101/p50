1078b6d113ce970e29779d38a7602e8484667d7f
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-17 23:20:07 +0100

    Update remove_LZF_filter.py

--- marker_genes/remove_LZF_filter.py
+++ marker_genes/remove_LZF_filter.py
@@ -1,5 +1,9 @@
-## This code removes 
+## This code removes LZF filter
+# This was not the issue for this dataset
+
 import anndata
 
+# https://github.com/mojaveazure/seurat-disk/issues/7
 adata = anndata.read("counts/GSE118127/local.h5ad")
 adata.write("counts/GSE118127/local.gzip.h5ad", compression="gzip")
+
