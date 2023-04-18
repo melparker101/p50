76ce272d49ad2e78163d573261969c1b03bdca96
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-18 18:09:16 +0100

    Update GSE118127_find_marker_genes.R

--- marker_genes/GSE118127_find_marker_genes.R
+++ marker_genes/GSE118127_find_marker_genes.R
@@ -1,42 +1,166 @@
-# We have 19 clusters for GSE118127
-# Find markers for these and write output as tables
-library(Seurat)
-library(SeuratDisk)
-library(SeuratObject)
+##############################################################################
+## Find marker genes for dataset GSE118127
+## Using a Wilcoxon Rank Sum test
+## melodyjparker@gmail.com - Apr 23
+##############################################################################
 
-in_file <- "counts/GSE118127/local.rds"
-out_dir <- "cluster_markers/GSE118127"
+##########################
+######### Set up #########
+##########################
 
 # Directly download the RDS 
 # https://cellxgene.cziscience.com/collections/2902f08c-f83c-470e-a541-e463e25e5058
 
-# Load in Seurat object
-GSE118127.seurat <- readRDS(file = in_file)
-GSE118127.seurat 
+library(Seurat)
+library(SeuratDisk)
+library(SeuratObject)
 
-# Have a look at the metadata
-GSE118127.seurat[[c('cluster_id','cell_description','cell_type')]][1:20,]
-table(GSE118127.seurat[['cluster_id']])
+# rm(list = ls())
 
-# Check current identity classes
-levels(GSE118127.seurat)
+dataset <- "GSE118127"
 
-# Set identity classes to the cell description column in meta data
-Idents(object = GSE118127.seurat) <- "cell_description"
-levels(GSE118127.seurat)
+in_file <- "counts/GSE118127/local.rds"
+out_dir <- paste0("cluster_markers/", dataset)
 
 # Create output directory
 dir.create(out_dir)
 
-# Find markers and write tables for each cluster
-cell_type_list <- levels(GSE118127.seurat)
+# Load in Seurat object
+seurat_ob <- readRDS(file = in_file)
+seurat_ob
+
+# Have a look at the metadata
+seurat_ob[[c('cluster_id','cell_description','cell_type')]][1:20,]
+table(seurat_ob[['cluster_id']])
+
+# cell_description corresponds to cluster_id
+
+# Create output directory
+dir.create(out_dir)
+
+# View metadata on feature level
+head(seurat_ob[['RNA']][[]])
+# Colnames are ensembl ids - we cannot change this
+# There is a feature_name column
+
+# The findAllMarkers uses the "data" slot, so change the rownames of this
+rownames(seurat_ob@assays$RNA@data) <- seurat_ob[['RNA']][['feature_name']][,1]
+
+##########################
+###### Find markers ######
+##########################
+
+### 1. Use cell_description (9 clusters)
+use_col <- "cell_type"
+
+# Set identity classes to the cell description
+Idents(object = seurat_ob) <- use_col
+levels(seurat_ob)
+
+# Create new directory for results of this cluster set
+clust_no <- paste0(length(levels(seurat_ob)),"C")
+cluster_dir <- paste(out_dir,clust_no,sep="/")
+dir.create(cluster_dir)
+
+# Find markers for every cluster compared to all remaining cells, report only the positive ones
+combined_markers <- FindAllMarkers(object = seurat_ob, 
+                          only.pos = TRUE,
+                          logfc.threshold = 0.25)  
+View(combined_markers)
+
+# pval = 0 just means the p-value is too small for R to display
+# https://github.com/satijalab/seurat/issues/205
+
+# Order the rows by clusters, then by p-adjusted values
+combined_markers <- combined_markers %>% arrange(as.character(cluster), as.numeric(as.character(p_val_adj)))
+combined_markers <- combined_markers %>% relocate(gene) %>% relocate(cluster)
+View(combined_markers)
+
+# Write as table
+combined_out <- paste0(cluster_dir,"/combined_markers_" , clust_no,".txt")
+write.table(combined_markers,combined_out,sep="\t",quote = FALSE)
+
+# Find top 5 markers per cluster
+top5_comb <- combined_markers %>%
+        group_by(cluster) %>%
+        top_n(n = 5,
+              wt = avg_log2FC)
+              
+# View top 5 markers per cluster
+View(top5_comb)
+
+# Write as table
+top5_comb_out <- paste0(cluster_dir,"/top5_comb_markers_",clust_no,".txt")
+write.table(top5_comb,top5_comb_out,sep="\t",quote = FALSE)
+
+# Check that there are no clusters with less than 3 cells
+table(seurat_ob$cell_type)
+
+# Find markers for each cluster and write to separate tables 
+cell_type_list <- levels(seurat_ob)
 for (cell_type in cell_type_list){
   name <- paste(cell_type,"markers",sep="_")
-  name <- sub(" ", "_", name)
-  name <- sub(")", "", name)
-  name <- sub("-", "_", name)
-  value <- FindMarkers(GSE118127.seurat, ident.1 = cell_type)
+  name <- gsub(" ", "_", name)
+  name <- gsub(")", "", name)
+  name <- gsub("-", "_", name)
+  name <- gsub("/", "_", name)
+  value <- FindMarkers(seurat_ob, ident.1 = cell_type)
   assign(name, value)
-  out <- paste(out_dir,name,sep="/")
-  write.table(value,out,sep="\t")
+  out <- paste0(cluster_dir,"/",name,"_",clust_no,".txt")
+  write.table(value,out,sep="\t",quote = FALSE)
 }
+
+### 2. Use cell_description (19 clusters)
+use_col <- "cell_description"
+
+# Set identity classes to the cell description
+Idents(object = seurat_ob) <- use_col
+levels(seurat_ob)
+
+# Create new directory for results of this cluster set
+clust_no <- paste0(length(levels(seurat_ob)),"C")
+cluster_dir <- paste(out_dir,clust_no,sep="/")
+dir.create(cluster_dir)
+
+# Find markers for every cluster compared to all remaining cells, report only the positive ones
+combined_markers <- FindAllMarkers(object = seurat_ob, 
+                          only.pos = TRUE,
+                          logfc.threshold = 0.25)  
+View(combined_markers)
+
+# Order the rows by clusters, then by p-adjusted values
+combined_markers <- combined_markers %>% arrange(as.character(cluster), as.numeric(as.character(p_val_adj)))
+combined_markers <- combined_markers %>% relocate(gene) %>% relocate(cluster)
+View(combined_markers)
+
+# Write as table
+combined_out <- paste0(cluster_dir,"/combined_markers_" , clust_no,".txt")
+write.table(combined_markers,combined_out,sep="\t",quote = FALSE)
+
+# Find top 5 markers per cluster
+top5_comb <- combined_markers %>%
+        group_by(cluster) %>%
+        top_n(n = 5,
+              wt = avg_log2FC)
+              
+# View top 5 markers per cluster
+View(top5_comb)
+
+# Write as table
+top5_comb_out <- paste0(cluster_dir,"/top5_comb_markers_",clust_no,".txt")
+write.table(top5_comb,top5_comb_out,sep="\t",quote = FALSE)
+
+# Find markers for each cluster and write to separate tables 
+cell_type_list <- levels(seurat_ob)
+for (cell_type in cell_type_list){
+  name <- paste(cell_type,"markers",sep="_")
+  name <- gsub(" ", "_", name)
+  name <- gsub(")", "", name)
+  name <- gsub("-", "_", name)
+  name <- gsub("/", "_", name)
+  value <- FindMarkers(seurat_ob, ident.1 = cell_type)
+  assign(name, value)
+  out <- paste0(cluster_dir,"/",name,"_",clust_no,".txt")
+  write.table(value,out,sep="\t",quote = FALSE)
+}
+
