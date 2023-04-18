e5954b3bacc91682167ad6fece1a567def563427
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-18 14:19:48 +0100

    Update GSE213216_find_marker_genes.R

--- marker_genes/GSE213216_find_marker_genes.R
+++ marker_genes/GSE213216_find_marker_genes.R
@@ -1,13 +1,17 @@
+# Find marker genes for dataset GSE213216
+# Using a wilcox test
 
 library(Seurat)
 library(SeuratDisk)
-library(SeuratObject)
+# library(SeuratObject)
 library(dplyr)
+
+# rm(list = ls())
 
 dataset <- "GSE213216"
 
 in_file <- "counts/GSE213216/counts_GSE213216.h5Seurat"
-out_dir <- "cluster_markers/GSE213216"
+out_dir <- paste0("cluster_markers/", dataset)
 
 # Create output directory
 dir.create(out_dir)
@@ -23,14 +27,19 @@ table(seurat_ob[['active_cluster']])
 
 # Remove doublets
 table(seurat_ob[['DF.classifications']])
-doub_rem <- subset(seurat_ob, subset=DF.classifications %in% "Singlet")
+seurat_ob <- subset(seurat_ob, subset=DF.classifications %in% "Singlet")
+dim(seurat_ob)
 
 ##### Find Markers #####
 
 ### 1. Use active clusters (9 clusters)
 clust_no <- "9C"
 
-# Set indentity classes as the pre-calculated Seurat clusters
+# Create new directory for results of 9 clusters
+cluster_dir <- paste(out_dir,clust_no,sep="/")
+dir.create(cluster_dir)
+
+# Set indentity classes as the active clusters
 Idents(object = seurat_ob) <- "active_cluster"
 levels(seurat_ob)
 
@@ -41,14 +50,15 @@ combined_markers <- FindAllMarkers(object = seurat_ob,
 View(combined_markers)
 
 # Order the rows by clusters, then by p-adjusted values
-combined_markers <- combined_markers %>% arrange(as.numeric(as.character(cluster)), as.numeric(as.character(p_val_adj)))
+combined_markers <- combined_markers %>% arrange(as.character(cluster), as.numeric(as.character(p_val_adj)))
 combined_markers <- combined_markers %>% relocate(gene) %>% relocate(cluster)
 View(combined_markers)
 
 # Write as table
-combined_out <- paste0(out_dir,"/combined_markers_",clust_no,".txt")
-write.table(combined_markers,combined_out,sep="\t")
+combined_out <- paste0(cluster_dir,"/combined_markers_" , clust_no,".txt")
+write.table(combined_markers,combined_out,sep="\t",quote = FALSE)
 
+# Find top 5 markers per cluster
 top5_comb <- combined_markers %>%
         group_by(cluster) %>%
         top_n(n = 5,
@@ -58,54 +68,71 @@ top5_comb <- combined_markers %>%
 View(top5_comb)
 
 # Write as table
-top5_comb_out <- paste0(out_dir,"/top5_comb_markers_",clust_no,".txt")
-write.table(top5_comb,top5_comb_out,sep="\t")
+top5_comb_out <- paste0(cluster_dir,"/top5_comb_markers_",clust_no,".txt")
+write.table(top5_comb,top5_comb_out,sep="\t",quote = FALSE)
 
 # Find markers for each cluster and write to separate tables 
 cell_type_list <- levels(seurat_ob)
 for (cell_type in cell_type_list){
   name <- paste(cell_type,"markers",sep="_")
-  name <- sub(" ", "_", name)
-  name <- sub(")", "", name)
-  name <- sub("-", "_", name)
+  name <- gsub(" ", "_", name)
+  name <- gsub(")", "", name)
+  name <- gsub("-", "_", name)
+  name <- gsub("/", "_", name)
   value <- FindMarkers(seurat_ob, ident.1 = cell_type)
   assign(name, value)
-  out <- paste0(out_dir,"/",name,"_",clust_no,".txt")
-  write.table(value,out,sep="\t")
+  out <- paste0(cluster_dir,"/",name,"_",clust_no,".txt")
+  write.table(value,out,sep="\t",quote = FALSE)
 }
 
 ### 2. Use seurat_clusters (70 clusters after doublet removal)
 clust_no <- "70C"
 
-# Set indentity classes as active clusters
-Idents(object = seurat_ob) <- "active_clusters"
+# Create new directory for results of 9 clusters
+cluster_dir <- paste(out_dir,clust_no,sep="/")
+dir.create(cluster_dir)
+
+# Set indentity classes as pre-calculated seurat clusters
+Idents(object = seurat_ob) <- "seurat_clusters"
 levels(seurat_ob)
 
 # Find all markers
 combined_markers <- FindAllMarkers(object = seurat_ob, 
                           only.pos = TRUE,
                           logfc.threshold = 0.25)
-                          
+
 # Order the rows by clusters, then by p-adjusted values
-combined_markers <- combined_markers %>%
-        dplyr::arrange(as.numeric(as.character(cluster)), as.numeric(as.character(p_val_adj)))
+combined_markers <- combined_markers %>% arrange(as.numeric(as.character(cluster)), as.numeric(as.character(p_val_adj)))
+combined_markers <- combined_markers %>% relocate(gene) %>% relocate(cluster)
 View(combined_markers)
                           
 # Write as table
-combined_out <- paste0(out_dir,"/combined_markers_",clust_no)
-write.table(value,combined_out,sep="\t")
+combined_out <- paste0(cluster_dir,"/combined_markers_" , clust_no,".txt")
+write.table(combined_markers,combined_out,sep="\t",quote = FALSE)
+
+# Find top 5 markers per cluster
+top5_comb <- combined_markers %>%
+        group_by(cluster) %>%
+        top_n(n = 5,
+              wt = avg_log2FC)
+              
+# Visualize top 5 markers per cluster
+View(top5_comb)
+
+# Write as table
+top5_comb_out <- paste0(cluster_dir,"/top5_comb_markers_",clust_no,".txt")
+write.table(top5_comb,top5_comb_out,sep="\t",quote = FALSE)
 
 # Find markers for each cluster and write to separate tables 
 cell_type_list <- levels(seurat_ob)
 for (cell_type in cell_type_list){
   name <- paste(cell_type,"markers",sep="_")
-  name <- sub(" ", "_", name)
-  name <- sub(")", "", name)
-  name <- sub("-", "_", name)
+  name <- gsub(" ", "_", name)
+  name <- gsub(")", "", name)
+  name <- gsub("-", "_", name)
+  name <- gsub("/", "_", name)
   value <- FindMarkers(seurat_ob, ident.1 = cell_type)
   assign(name, value)
-  out <- paste0(out_dir,"/",name,"_",clust_no)
-  write.table(value,out,sep="\t")
+  out <- paste0(cluster_dir,"/",name,"_",clust_no,".txt")
+  write.table(value,out,sep="\t",quote = FALSE)
 }
-
-# Done
