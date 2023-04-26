544d0f41cf97215aaf9fdb85af274a4026311fc4
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-26 14:15:17 +0100

    Update GSE213216_find_marker_genes.R

--- marker_genes/GSE213216_find_marker_genes.R
+++ marker_genes/GSE213216_find_marker_genes.R
@@ -7,6 +7,11 @@
 # Use the original _aux.seurat.shared.rds because it contains the normalised counts
 # They used SCTransform to normalise the data
 # Check if this is still normally distributed for our filtered subset
+# Only use active clusters (there are too many seurat clusters)
+
+# Run this for
+# 1. The already processed (SCT-normalized) dataset. Filter and then find markers
+# 2. The filtered (for CELLEX) raw counts. Normalise these (log normalization), then find markers
 
 ################################
 # Load libraries
@@ -36,9 +41,9 @@ plotClusters <- function(object,clusters,out){
 }
 
 ################################
-# Set up 
+# 1. SCTransform normalisation
 ################################
-
+##### Set up #####
 # rm(list = ls())
 
 # Dataset accession
@@ -50,27 +55,19 @@ out_dir <- paste0("cluster_markers/", dataset)
 # Create output directory
 dir.create(out_dir)
 
-### 1. Set up for SCTransform normalisation ###
-###############################################
-
-# Input file - processed, unfiltered dataset
+# Input file - dataset is processed (already normalised) and unfiltered (for the samples we want to keep)
 seurat_in <- paste0("counts/",dataset,"/_aux.seurat.shared.rds")
 
 # Load in Seurat object
 seurat_ob <- readRDS(file = seurat_in)
 seurat_ob
 
-### 1. Set up for log normalisation ###
-###############################################
+# View the total RNA count per cell and group by clusters
+# VlnPlot(object = seurat_ob, features = c("nFeature_RNA"), group.by = c('seurat_clusters'))
+VlnPlot(object = seurat_ob, features = c("nFeature_RNA"), group.by = c('active.cluster'))
 
-# Input file - raw, filtered dataset (created for input to CELLEX)
-seurat_in <- paste0("counts/", dataset, "/counts_GSE213216.h5Seurat")
+##### Filtering #####
 
-# seurat_ob <- 
-
-################################
-# Filtering
-################################
 # Have a look at the colnames of the metadata
 colnames(seurat_ob[[]])
 
@@ -110,92 +107,17 @@ dim(seurat_ob)
 # 30354 22219
 # We now have 22,219 cells
 
-################################
-# Check if subset is normally distributed
-################################
-# https://stackoverflow.com/questions/29287614/r-normalize-then-plot-two-histograms-together-in-r
-# Plot a histogram
-
-# Fake data (two normal distributions)
-set.seed(20)
-dat1 = data.frame(x=rnorm(1000, 100, 10), group="A")
-dat2 = data.frame(x=rnorm(2000, 120, 20), group="B")
-dat = rbind(dat1, dat2)
-
-ggplot(dat, aes(x, fill=group, colour=group)) +
-  geom_histogram(breaks=seq(0,200,5), alpha=0.6, 
-                 position="identity", lwd=0.2) +
-  ggtitle("Unormalized")
-
-ggplot(dat, aes(x, fill=group, colour=group)) +
-  geom_histogram(aes(y=..density..), breaks=seq(0,200,5), alpha=0.6, 
-                 position="identity", lwd=0.2) +
-  ggtitle("Normalized")
-
-
-#########################################################
-
-# Renormalise
-# Set raw counts to default
-DefaultAssay(seurat_ob) <- "RNA"
-# Remove
-seurat_ob[['SCT']]<-NULL
-seurat_ob
-
-# https://satijalab.org/seurat/archive/v3.2/integration.html
-# https://satijalab.org/seurat/articles/sctransform_v2_vignette.html
-
-
-GetAssayData(seurat_ob)[1:20,1:20]
-
-
-
-
-
-# Create a new Seurat object using only the raw data - otherwise raw count data won't convert to anndata properly
-raw <- GetAssayData(object = seurat_ob, slot = "counts")
-metadata <- seurat_ob[[]]
-seurat_obj <- CreateSeuratObject(counts=raw, meta.data=metadata)
-
-seurat_obj <- SCTransform(seurat_obj, vars.to.regress = "percent.mito", verbose = FALSE)
-
-
-  n <- length(levels(seurat_obj))
-  clust_no <- paste0(n,"C")
-  # cluster.cols = as.vector(as.vector(tol.rainbow(n)))
-  cluster.cols = as.vector(polychrome(n))
-  plot = DimPlot(object = seurat_obj, 
-               pt.size = 0.1, 
-               raster=FALSE, 
-               group.by = "active_cluster",
-               cols = cluster.cols,
-               label = T)
-plot
-
-
-######################################################
-
-# View metadata
+# See how many clusters there are for different cluster types
+dim(unique(seurat_ob[['seurat_clusters']]))[1]
 seurat_ob[[c('seurat_clusters','active_cluster')]][1:20,]
-table(seurat_ob[['seurat_clusters']])
-table(seurat_ob[['active_cluster']])
+dim(unique(seurat_ob[['active_cluster']]))[1]
 
-# Remove doublets
-# table(seurat_ob[['DF.classifications']])
-# seurat_ob <- subset(seurat_ob, subset=DF.classifications %in% "Singlet")
-# dim(seurat_ob)
+##### Find markers #####
 
-# View the total RNA count per cell and group by clusters
-VlnPlot(object = seurat_ob, features = c("nFeature_RNA"), group.by = c('seurat_clusters'))
-VlnPlot(object = seurat_ob, features = c("nFeature_RNA"), group.by = c('active_cluster'))
-
-################################
-# Find markers
-################################
-
-### 1. Use active clusters (9 clusters)
+# Use active clusters (9 clusters)
 # Cluster names are character
 # Number of clusters = 9
+norm_method = "SCT"
 
 # Set indentity classes as the active clusters
 use_col <- "active_cluster"
@@ -203,7 +125,7 @@ Idents(object = seurat_ob) <- use_col
 levels(seurat_ob)
 
 # Create new directory for results of this cluster set
-clust_no <- paste0(length(levels(seurat_ob)),"C")
+clust_no <- paste0(length(levels(seurat_ob)),"C_",norm_method)
 cluster_dir <- paste(out_dir,clust_no,sep="/")
 dir.create(cluster_dir)
 
@@ -211,12 +133,12 @@ dir.create(cluster_dir)
 combined_markers <- FindAllMarkers(object = seurat_ob, 
                           only.pos = TRUE,
                           logfc.threshold = 0.25)  
-View(combined_markers)
+# View(combined_markers)
 
-# Order the rows by clusters, then by p-adjusted values
-combined_markers <- combined_markers %>% arrange(as.character(cluster), as.numeric(as.character(p_val_adj)))
+# Order the rows by clusters, then by p-val
+combined_markers <- combined_markers %>% arrange(as.character(cluster), as.numeric(as.character(p_val)))
 combined_markers <- combined_markers %>% relocate(gene) %>% relocate(cluster)
-View(combined_markers)
+# View(combined_markers)
 
 # Write as table
 combined_out <- paste0(cluster_dir,"/combined_markers_" , clust_no,".txt")
@@ -230,22 +152,126 @@ top5_comb <- combined_markers %>%
 View(top5_comb)
 
 # Write as table
-top5_comb_out <- paste0(cluster_dir,"/top5_comb_markers_",clust_no,".txt")
+top5_comb_out <- paste0(cluster_dir,"/top5_avglog2FC_comb_markers_",clust_no,".txt")
 write.table(top5_comb,top5_comb_out,sep="\t",quote = FALSE)
 
 # Find markers for each cluster and write to separate tables 
 cell_type_list <- levels(seurat_ob)
 for (cell_type in cell_type_list){
+  # Extract and edit cell type names
   name <- paste(cell_type,"markers",sep="_")
   name <- gsub(" ", "_", name)
   name <- gsub(")", "", name)
   name <- gsub("-", "_", name)
   name <- gsub("/", "_", name)
+  # Find markers
   value <- FindMarkers(seurat_ob, ident.1 = cell_type, only.pos = TRUE)
+  # Reformat marker results table - order by pval and change col order
+  value <- value %>% arrange(as.numeric(as.character(p_val)))
   assign(name, value)
+  # Write to file
   out <- paste0(cluster_dir,"/",name,"_",clust_no,".txt")
   write.table(value,out,sep="\t",quote = FALSE)
 }
 
 # Plot clusters
-plotClusters(seurat_ob,use_col,cluster_dir)
+# plotClusters(seurat_ob,use_col,cluster_dir)
+
+################################
+# Log normalisation
+################################
+##### Set up #####
+
+rm(list = ls())
+
+# Dataset accession
+dataset <- "GSE213216"
+
+# Out dir
+out_dir <- paste0("cluster_markers/", dataset)
+
+# Input file - raw, filtered dataset (created for input to CELLEX)
+seurat_in <- paste0("counts/", dataset, "/counts_GSE213216.h5Seurat")
+
+# Read in Seurat object (this is already filtered for the samples we want to use)
+seurat_ob <- LoadH5Seurat(seurat_in)
+seurat_ob
+
+# Check that this is already filtered
+dim(seurat_ob)
+# 30354 22219
+
+# View raw counts
+GetAssayData(seurat_ob)[1:20,1:20]
+
+# Normalise
+seurat_ob <- NormalizeData(seurat_ob, normalization.method = "LogNormalize", scale.factor = 10000)
+
+# View normalised counts
+GetAssayData(seurat_ob)[1:20,1:20]
+
+##### Find markers #####
+
+# Use active clusters (9 clusters)
+# Cluster names are character
+# Number of clusters = 9
+norm_method = "log"
+
+# Set indentity classes as the active clusters
+use_col <- "active_cluster"
+Idents(object = seurat_ob) <- use_col
+levels(seurat_ob)
+
+# Create new directory for results of this cluster set
+clust_no <- paste0(length(levels(seurat_ob)),"C_",norm_method)
+cluster_dir <- paste(out_dir,clust_no,sep="/")
+dir.create(cluster_dir)
+
+# Find markers for every cluster compared to all remaining cells, report only the positive ones
+combined_markers <- FindAllMarkers(object = seurat_ob, 
+                          only.pos = TRUE,
+                          logfc.threshold = 0.25)  
+# View(combined_markers)
+
+# Order the rows by clusters, then by p-val
+combined_markers <- combined_markers %>% arrange(as.character(cluster), as.numeric(as.character(p_val)))
+combined_markers <- combined_markers %>% relocate(gene) %>% relocate(cluster)
+# View(combined_markers)
+
+# Write as table
+combined_out <- paste0(cluster_dir,"/combined_markers_" , clust_no,".txt")
+write.table(combined_markers,combined_out,sep="\t",quote = FALSE)
+
+# Find top 5 markers per cluster
+top5_comb <- combined_markers %>%
+        group_by(cluster) %>%
+        top_n(n = 5,
+              wt = avg_log2FC)
+View(top5_comb)
+
+# Write as table
+top5_comb_out <- paste0(cluster_dir,"/top5_avglog2FC_comb_markers_",clust_no,".txt")
+write.table(top5_comb,top5_comb_out,sep="\t",quote = FALSE)
+
+# Find markers for each cluster and write to separate tables 
+cell_type_list <- levels(seurat_ob)
+for (cell_type in cell_type_list){
+  # Extract and edit cell type names
+  name <- paste(cell_type,"markers",sep="_")
+  name <- gsub(" ", "_", name)
+  name <- gsub(")", "", name)
+  name <- gsub("-", "_", name)
+  name <- gsub("/", "_", name)
+  # Find markers
+  value <- FindMarkers(seurat_ob, ident.1 = cell_type, only.pos = TRUE)
+  # Reformat marker results table - order by pval and change col order
+  value <- value %>% arrange(as.numeric(as.character(p_val)))
+  assign(name, value)
+  # Write to file
+  out <- paste0(cluster_dir,"/",name,"_",clust_no,".txt")
+  write.table(value,out,sep="\t",quote = FALSE)
+}
+
+################################
+# End
+################################
