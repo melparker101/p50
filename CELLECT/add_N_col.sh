037ca09bb7606d3e1aab0eeb46b7f29609c258ab
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-27 15:22:16 +0100

    Update add_N_col.sh

--- CELLECT/add_N_col.sh
+++ CELLECT/add_N_col.sh
@@ -6,6 +6,9 @@ ORIG_SUMSTATS=data/sumstats/original
 # Subset the sumstats data to only include the 'MarkerName' and 'Direction' columns
 awk '{print $1, $11}' "$ORIG_SUMSTATS"/female_infertility_analysis1_UKBB_Finngen_EstBB_noMACfilter_March20231.out > "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt
 
+# Try this so that the colname for direction doesn't split
+# awk 'NR==1 {print $1 "hello", "hello2", "hello3",; next} NR==FNR -F" " '{printf "%s\t", $1; gsub(/.{1}/,"& ",$2); print $2}' "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt > tmp && mv tmp "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt
+
 # Split up the characters of the 'Direction' column and overwrite file
 awk -F" " '{printf "%s\t", $1; gsub(/.{1}/,"& ",$2); print $2}' "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt > tmp && mv tmp "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt
 
