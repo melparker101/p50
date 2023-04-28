abd4be12c34002662836c4e540a9f6aa8ba2e0a3
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-28 11:19:16 +0100

    Update add_N_col.sh

--- CELLECT/add_N_col.sh
+++ CELLECT/add_N_col.sh
@@ -1,15 +1,16 @@
+####################################
+# melodyjparker14@gmail.com
+####################################
+
 # Run from p50 for now
 
 # Path for sumstats files
-ORIG_SUMSTATS=data/sumstats/original
+IN=data/sumstats/original
+OUT=data/sumstats/not_using
 
 # Subset the sumstats data to only include the 'MarkerName' and 'Direction' columns
-awk '{print $1, $11}' "$ORIG_SUMSTATS"/female_infertility_analysis1_UKBB_Finngen_EstBB_noMACfilter_March20231.out > "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt
+awk '{print $1, $11}' "$IN"/female_infertility_analysis1_UKBB_Finngen_EstBB_noMACfilter_March20231.out > "$OUT"/Infertility1_F_EUR_directions.txt
 
-# Try this so that the colname for direction doesn't split
-# awk 'NR==1 {print $1 "hello", "hello2", "hello3",; next} NR==FNR -F" " '{printf "%s\t", $1; gsub(/.{1}/,"& ",$2); print $2}' "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt > tmp && mv tmp "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt
-
-# Split up the characters of the 'Direction' column and overwrite file
-awk -F" " '{printf "%s\t", $1; gsub(/.{1}/,"& ",$2); print $2}' "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt > tmp && mv tmp "$ORIG_SUMSTATS"/Infertility1_F_EUR_directions.txt
-
-# Infertility1_F_EUR.txt
+# Split up the characters of the 'Direction' column and name columns by study name
+# Overwrite file
+awk 'NR==1 {print $1, "FinnGen", "UKBB", "EstBB"; next} NR==FNR {printf "%s\t", $1; gsub(/.{1}/,"& ",$2); print $2}' "$OUT"/Infertility1_F_EUR_directions.txt > tmp && mv tmp "$OUT"/Infertility1_F_EUR_directions.txt
