d4056a813e9ac3e0143c538d1f11f288ef7fa3ac
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-28 15:31:06 +0100

    Update add_N_col.R

--- CELLECT/add_N_col.R
+++ CELLECT/add_N_col.R
@@ -45,5 +45,23 @@ EstBB_N_eff <- calcNeff(EstBB_N_Cases, EstBB_N_Controls)  # 21710.03
 infert2 <- infert %>% 
   mutate(N = (direc$FinnGen * FinnGen_N_eff) + (direc$UKBB * UKBB_N_eff) + (direc$EstBB * EstBB_N_eff))
 
-timing <- system.time(fwrite(infert2, out_file, sep=" "))
-timing
+infert3 <- infert2 %>% 
+  mutate(N2 = calcNeff( (direc$FinnGen*FinnGen_N_Cases + direc$UKBB*UKBB_N_Cases + direc$EstBB*EstBB_N_Cases), (direc$FinnGen*FinnGen_N_Controls + direc$UKBB*UKBB_N_Controls + direc$EstBB*EstBB_N_Controls)))
+
+# It can be shown that by splitting the case-control data into subsets, the sum of the effective sample sizes over the subsets is always ≤ the effective sample size of the whole data
+# https://www.mv.helsinki.fi/home/mjxpirin/GWAS_course/material/GWAS9.html
+
+# Make a scatter plot of the two N-eff columns for the different combinations of studies
+df <- data.frame (FinnGen  = c(1,0,1,1),
+                  UKBB = c(1,1,0,1),
+                  EstBB = c(0,1,1,1)
+                  )
+
+df <- df %>% 
+  mutate(N = (FinnGen * FinnGen_N_eff) + (UKBB * UKBB_N_eff) + (EstBB * EstBB_N_eff)) %>%
+  mutate(N2 = calcNeff( (FinnGen*FinnGen_N_Cases + UKBB*UKBB_N_Cases + EstBB*EstBB_N_Cases), (FinnGen*FinnGen_N_Controls + UKBB*UKBB_N_Controls + EstBB*EstBB_N_Controls)))
+  
+plot(df$N,df$N2, main="Comparison of N-eff when calculated in two different ways",
+   xlab="N ", ylab="N2 ", pch=19)
+
+fwrite(infert3,outfile,sep=" ")
