da54ded53eb9e789e9bba6673aa133c7028aaa59
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-04-28 11:26:25 +0100

    Update add_N_col.sh

--- CELLECT/add_N_col.sh
+++ CELLECT/add_N_col.sh
@@ -1,8 +1,9 @@
-####################################
-# melodyjparker14@gmail.com
-####################################
+########################################
+# melodyjparker14@gmail.com - Apr 23
+########################################
+# Make a text file with a marker column and a seperate direction column for each study
 # Run from p50 for now
-# Use awk to edit as it is very efficient
+# Use awk to edit as it is efficient
 
 # Path for sumstats files
 IN=data/sumstats/original
