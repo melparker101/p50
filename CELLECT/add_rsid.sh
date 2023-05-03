189efa35bf225ba92ef1529cc6a8fde5e47b3b18
Melody Parker <98864236+melparker101@users.noreply.github.com>
2023-05-03 15:15:04 +0100

    Update add_rsid.sh

--- CELLECT/add_rsid.sh
+++ CELLECT/add_rsid.sh
@@ -33,19 +33,20 @@ echo "##########################################################"
 # cd = //well/lindgren/users/mzf347/p50
 
 # Set variables
-IN=sumstats/MAF_filtered
-OUT=sumstats/premunge
+IN=data/sumstats/other
+OUT=data/sumstats/other
+# OUT=data/sumstats/premunge
 MAP=dbSNP
 
 # Use GRCh37_GCF_000001405.25_map.txt to get more rsids
 
 # Make an index file if it doesn't exist
 if [ ! -f "$IN"/index.txt ]; then
-  for f in "$IN"/*; do basename ${f} >> "$IN"/index.txt; done
+  for f in "$IN"/MAFfiltered*; do basename ${f} >> "$IN"/index.txt; done
 fi
 
 # Make out dir if it doesn't exist
-mkdir -p "$OUT"
+# mkdir -p "$OUT"
 
 # Set sumstats file name for each slurm array task
 SUMSTATS_FILE=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' "$IN"/index.txt)
@@ -53,7 +54,7 @@ SUMSTATS_FILE=$(sed "${SLURM_ARRAY_TASK_ID}"'q;d' "$IN"/index.txt)
 # Add rsid column and save in new folder
 awk 'NR==1 {print "RSID",$0; next} NR==FNR {a[$1]=$0; next} $1 in a {print $2,a[$1]}' \
     "$IN"/"$SUMSTATS_FILE" "$MAP"/MarkerName_map_GRCh37.txt > \
-    "$OUT"/premunge_"${SUMSTATS_FILE##*MAFfiltered_}"
+    "$OUT"/premunge_"${SUMSTATS_FILE##*MAFfiltered_MN_Ncol_}"
 
 
 echo "###########################################################"
